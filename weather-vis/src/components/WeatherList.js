b4888b627e29c1d5cc0a3c47b945546f50eb8146
zubin | zeuscoder <zubin.pratap@gmail.com>
2018-12-12 12:03:59 +1100

    implement temperature chart. working

--- weather-vis/src/components/WeatherList.js
+++ weather-vis/src/components/WeatherList.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+//REFERENCE:  https://github.com/borisyankov/react-sparklines
 
 class WeatherList extends Component {
   renderWeatherList = cityData => {
+    const temps = cityData.list.map(weather => {
+      return weather.main.temp;
+    });
+    const winds = cityData.list.map(weather => {
+      return weather.wind.speed;
+    });
+    const humidity = cityData.list.map(weather => {
+      return weather.main.humidity;
+    });
+
     return (
       <tr key={`${cityData.city.name}_${cityData.city.id}`}>
         <td>{cityData.city.name}</td>
-        <td>{cityData.list[0].main.temp}</td>
-        <td>{cityData.list[0].wind.speed}</td>
-        <td>{cityData.list[0].main.humidity}</td>
+        {/* <td>{cityData.list[0].main.temp}</td> */}
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="red" />
+          </Sparklines>
+        </td>
+        {/* <td>{cityData.list[0].wind.speed}</td>
+        <td>{cityData.list[0].main.humidity}</td> */}
       </tr>
     );
   };
